716372a2b5487866d752a29911366e5b6fff57a0
zhanghewen <2785015696@qq.com>
2023-06-27 10:12:14 +0800

    开发

--- js/index-guide.js
+++ js/index-guide.js
@@ -1,3 +1,4 @@
+$('title').html('一事一码-办事指南')
 $(document).ready(function () {
     $('tbody tr').each(function () {
         var $td = $(this).find('td');
@@ -72,9 +73,44 @@ $(".table-tab1").html(table_tab1_html);
 
 //办事指南tab2 受理条件
 var table_tab2_html='<div class="tab2Text">'+eventItem.workAttention.HandlingCondition.HandlingCondition+'</div>';
-console.log(table_tab2_html);
 $(".table-tab2").html(table_tab2_html);
 
+//tab3办理流程
+// 需要遍历这些流程
+console.log(eventItem.workAttention.handlingProcess);
+var table_tab3_html='';
+$.each(eventItem.workAttention.handlingProcess, function (index, handlingProcess) {
+    table_tab3_html +=
+    '<tr><td><div>'+handlingProcess.processName+'</div></td>'+
+    '<td><div>'+handlingProcess.Procedure+'</div></td>'+
+    '<td><div>'+handlingProcess.worker+'</div></td>'+
+    '<td><div>'+handlingProcess.limitTime+'</div></td>'+
+    '<td><div>'+handlingProcess.checkStandard+'</div></td>'+
+    '<td><div>'+handlingProcess.result+'</div></td></tr>'
+});
+$(".table-tab3").html(table_tab3_html);
 
+//tab04办事地点
+$(".windowsName").html(eventItem.workAttention.handingAddress.windowsName);
+$(".windowsPhone").html(eventItem.workAttention.handingAddress.windowsPhone);
+$(".windowsTime").html(eventItem.workAttention.handingAddress.windowsTime);
+$(".windowsAddress").html(eventItem.workAttention.handingAddress.windowsAddress);
+$(".trafficGuide").html(eventItem.workAttention.handingAddress.trafficGuide);
+
+//tab5申请材料
+
+// 需要遍历这些流程
+console.log(eventItem.workAttention.applicationMaterial);
+var table_tab5_html='';
+$.each(eventItem.workAttention.applicationMaterial, function (index, applicationMaterial) {
+    table_tab5_html +=
+        '<tr><td><div>'+applicationMaterial.materialName+'</div></td>'+
+        '<td><div>'+applicationMaterial.materialExample+'</div></td>'+
+        '<td><div>'+applicationMaterial.materialSource+'</div></td>'+
+        '<td><div>'+applicationMaterial.materialType+'</div></td>'+
+        '<td><div>'+applicationMaterial.materialNeed+'</div></td>'+
+        '<td><div>'+applicationMaterial.materialStandard+'</div></td></tr>'
+});
+$(".table-tab5").html(table_tab5_html);
 
 
